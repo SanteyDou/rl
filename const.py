ee0b737abe477209b551af146b8d46951e3f5971
SanteyUA <santey.ua@gmail.com>
2018-02-21 15:33:41 +0200

    episode 2

---
+++ const.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+
+#game sizes
+GAME_WIDTH = 800
+GAME_HEIGHT = 600
+
+#Color definitions
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_GREY = (100, 100, 100)
+COLOR_GRAPHITE = (43, 43, 43)
+
+#Game colors
+COLOR_DEFAULT_BG = COLOR_GRAPHITE
+
+#SPRITES
+S_PLAYER = pygame.image.load("assets/python.png")
--- main.py
+++ main.py
@@ -1,4 +1,54 @@
-import pygame
 import libtcodpy as libtcod
+import pygame, sys, random
 
-print('Hello world!')
+import const
+
+def draw_game():
+
+    global SURFACE_MAIN
+
+    #clear the surface
+    SURFACE_MAIN.fill(const.COLOR_DEFAULT_BG)
+
+    #TODO draw the map
+
+    #draw the character
+    SURFACE_MAIN.blit(const.S_PLAYER, ( const.GAME_WIDTH/2-16, const.GAME_HEIGHT/2-16 ))
+
+    #update the display
+    pygame.display.flip()
+
+
+def game_main_loop():
+
+    game_quit = False
+
+    while not game_quit:
+
+        #get player input
+        events_list = pygame.event.get()
+
+        #process input
+        for event in events_list:
+            if event.type == pygame.QUIT:
+                game_quit = True
+
+        #draw the game
+        draw_game()
+
+    #quit the game
+    pygame.quit()
+    exit()
+
+def game_init():
+
+    global SURFACE_MAIN
+
+    #initialize pygame
+    pygame.init()
+
+    SURFACE_MAIN = pygame.display.set_mode( (const.GAME_WIDTH, const.GAME_HEIGHT) )
+
+if __name__ == '__main__':
+    game_init()
+    game_main_loop()
